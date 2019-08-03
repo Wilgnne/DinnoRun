e2f2d79f982913046620723677f8d58e7105e9f7
Wilgnne <w.khawanbarbosa@gmail.com>
2019-08-03 18:14:52 -0300

    Map

---
+++ Assets/BlockControll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class BlockControll : MonoBehaviour
+{
+    BoxCollider2D box;
+    SpriteRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        box = GetComponent<BoxCollider2D>();
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (renderer.sprite == null)
+            box.enabled = false;
+        else
+            box.enabled = true;
+    }
+}
---
+++ Assets/GraundController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct GraundSettings
+{
+    public GameObject graundBase;
+    public List<Sprite> graundSprits;
+    public int size;
+    public SpriteRenderer[] theGraund;
+}
+
+[System.Serializable]
+public class PerlinNoiseSettings
+{
+    public float seed, scale, resolution;
+
+    public float CalculePerlin(float x)
+    {
+        float xCoord = seed + x / resolution * scale;
+        float yCoord = seed / resolution * scale;
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
+
+public class GraundController : MonoBehaviour
+{
+    public static List<GraundController> controllers;
+    public GraundSettings graundSettings;
+    public PerlinNoiseSettings noiseSettings;
+    public float minPerlinCont;
+    public AnimationCurve curve;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (controllers == null)
+            controllers = new List<GraundController>();
+        controllers.Add(this);
+        graundSettings.theGraund = new SpriteRenderer[graundSettings.size];
+        for(int i = 0; i < graundSettings.size; i++)
+        {
+            GameObject obj = Instantiate(graundSettings.graundBase, transform);
+            obj.transform.localPosition = new Vector3(i, 0);
+            graundSettings.theGraund[i] = obj.GetComponent<SpriteRenderer>();
+        }
+    }
+
+    public void Move(int posX)
+    {
+        Keyframe[] keys = new Keyframe[graundSettings.size];
+        for (int i = 0; i < graundSettings.size; i++)
+        {
+            float noise = noiseSettings.CalculePerlin(posX + i);
+            int index = 0;
+            if (noise > minPerlinCont)
+            {
+                index = Mathf.FloorToInt(noise * graundSettings.graundSprits.Count);
+            }
+
+            keys[i] = new Keyframe(i, index);
+
+            graundSettings.theGraund[i].sprite = graundSettings.graundSprits[index % graundSettings.graundSprits.Count];
+        }
+        curve = new AnimationCurve(keys);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
